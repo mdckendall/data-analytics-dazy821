ecc2b25392a72c1e5d99774d6ac423ecb58ba617
Kayla Ribeiro <77889723+dazy821@users.noreply.github.com>
2023-03-07 01:34:50 +0000

    4th attempt

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ class Main {
     
 while (true){
     System.out.println("Press 1 to learn about salary.");
-    System.out.println("Press 2 to learn about job.");
+    System.out.println("Press 2 to learn about the job.");
     System.out.println("Press 3 to learn about demand.");
     System.out.println("Press 4 to view current students.");
     System.out.println("Press 5 to quit.");
